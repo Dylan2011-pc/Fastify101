63a7457449d8a464aab7f4309f756cf17f809e00
Dylan2011-pc <duyl0043@gmail.com>
2021-11-24 10:32:38 +0700

    fix

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -44,7 +44,9 @@ const  senddata =async(event)=>{
 return (
 <div class="login-box">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-  <h2><i class="glyphicon glyphicon-user"></i>  Login</h2>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+  
+  <h2><i class="glyphicon glyphicon-user" ></i>  Login</h2>
   <form>
     <div class="user-box">
       <input type="text"  name ="email" value={setlogin.email} onChange={Laydata} required />
@@ -52,21 +54,21 @@ return (
     </div>
     <div class="user-box">
       <input type="password"  name="password" value={setlogin.password} onChange={Laydata} required/>
-      <label> <i class="fa fa-lock"></i> Password</label>
+      <label>   <i class="fa fa-lock"></i>   Password</label>
     </div>
     <div class="checkbox pull-left">
-          <label><input type="checkbox"  /> Remember</label>
+          <label><input type="checkbox"/> Remember</label>
     </div>
     <a href="#">
       <span></span>
-      <button  class="btn btn-lg btn-block " type="submit" variant="primary" > Login </button>
+      <button  class="btn btn-lg btn-block " type="submit" variant="primary" >LOGIN  <i class="fa fa-sign-in"></i></button>
       <span></span>
       <span></span>
       <span></span>
     </a>
      <a href="#">
       <span></span>
-      <button  class="btn btn-lg btn-block " type="submit" variant="primary" >Sign Up</button>
+      <button  class="btn btn-lg btn-block " type="submit" variant="primary" >SIGN UP  <i class="fa fa-user-plus"></i></button>
       <span></span>
       <span></span>
       <span></span>
