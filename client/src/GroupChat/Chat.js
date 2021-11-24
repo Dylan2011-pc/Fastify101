1b871e3acbe406659b663d6db1671d812a606841
Dylan2011-pc <duyl0043@gmail.com>
2021-11-24 21:14:41 +0700

    fix-loginform

--- client/src/GroupChat/Chat.js
+++ client/src/GroupChat/Chat.js
@@ -1,5 +1,7 @@
 import React ,{ useState } from 'react'
 import './Chat.css'
+
+
   export const PopChat = ( props ) => {
   let hide = {
     display: 'none',
@@ -20,24 +22,25 @@ const handleSend = e => {
   get(textRef.current.value)
 }
 
-//hamlogic o day
-
  
   return ( 
-      <div id='chatCon'>
-      <div class="chat-box" style={chatopen ? show : hide}>
+    
+  <div id='chatCon'>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+    <div class="chat-box" style={chatopen ? show : hide}>
     <div class="header">Chat box</div>
-    <div class="msg-area">
-    </div>
-    <div class="footer">
+    <div class="msg-area"></div>
+      <div class="footer">
       <input type="text"  ref={textRef} />
+      <label for="myfile"><i class="material-icons">attachment</i></label>
+      <input type="file" id="myfile" name="myfile"/>
       <button onClick={handleSend}><i class="fa fa-paper-plane"></i></button>
+      </div>
     </div>
-  </div>
     <div class="pop">
       <p><img onClick={toggle} src="https://www.freeiconspng.com/uploads/message-icon-png-4.png"  alt="" /></p>
     </div>
-    </div>
+  </div>
 
     
     )}
